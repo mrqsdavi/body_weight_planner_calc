1be205bd228de7a94899eb2c94bf768aad0dc306
Davi Marques <dmarques@staholding.com>
2020-02-25 11:19:43 -0300

    goal maintenance intervention

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Baseline = require('./baseline');
 const Intervention = require('./intervention');
+const BodyModel = require('./bodymodel');
 
 const ENERGYUNITS = 1;
 
@@ -7,10 +8,10 @@ const ENERGYUNITS = 1;
 const age = 27; // Ano
 const height = 170; // Centímetros
 const weight = 124; // Quilogramas
-const physicalActivityLevel = 1.6; // 1.4 - 2.3
-
-var goalTime = 90; //Dias para conseguir o objetivo.
+const physicalActivityLevel = 1.4; // 1.4 - 2.3
+var goalTime = 200; //Dias para conseguir o objetivo.
 var goalWeight = 100; // Quilogramas
+
 const baseline = new Baseline(true, age, height, weight, true, false, physicalActivityLevel);
 
 var goalIntervention = new Intervention();
@@ -30,5 +31,16 @@ try {
 }
 
 const goalCalsField = Math.round(goalIntervention.calories * ENERGYUNITS);
+let goalMaintCals = goalCalsField;
 
-console.log(goalCalsField, unachievableGoal);
+var goalbc = new BodyModel.projectFromBaselineViaIntervention(baseline, goalIntervention, parseInt(goalTime) + 1);
+var weightAtGoal = baseline.getNewWeightFromBodyModel(goalbc);
+var bfpAtGoal = goalbc.getFatPercent(baseline);
+
+if (goalWeight == baseline.weight && goalMaintenanceIntervention.actchangepercent == 0) {
+    goalMaintCals = Math.round(baseline.getMaintCals());
+} else {
+    goalMaintCals = Math.round(goalbc.cals4balance(baseline, goalMaintenanceIntervention.getAct(baseline)));
+}
+
+console.log(goalCalsField, unachievableGoal, goalMaintCals);
