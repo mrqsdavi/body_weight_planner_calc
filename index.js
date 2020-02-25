854d407daba19473ec63cc6b062037cbc05e952d
Davi Marques <dmarques@staholding.com>
2020-02-25 10:26:32 -0300

    unused code removed

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const moment = require('moment');
 const Baseline = require('./baseline');
 const Intervention = require('./intervention');
 
-const WEIGHTUNITS = 1;
 const ENERGYUNITS = 1;
 
-
 //Entradas
-const age = 27; // Years
-const height = 170; // Centimeters
-const weight = 124; // kilograms
-const physicalActivityLevel = 1.4; // 1.4 - 2.3
+const age = 27; // Ano
+const height = 170; // Centímetros
+const weight = 124; // Quilogramas
+const physicalActivityLevel = 1.6; // 1.4 - 2.3
 
-
-
-
-var goalTime = 200; //Dias para conseguir o objetivo.
-var goalWeight = 100;
+var goalTime = 90; //Dias para conseguir o objetivo.
+var goalWeight = 100; // Quilogramas
 const baseline = new Baseline(true, age, height, weight, true, false, physicalActivityLevel);
-
-var healthyWeightRange = baseline.getHealthyWeightRange();
-const weightRangeLow = Math.floor(healthyWeightRange.low * WEIGHTUNITS);
-const weightRangeHigh = Math.ceil(healthyWeightRange.high * WEIGHTUNITS);
-
-const dt = moment(new Date()).add(parseInt(goalTime), 'days').format('M/D/YYYY');
 
 var goalIntervention = new Intervention();
 var goalMaintenanceIntervention = goalIntervention;
@@ -43,4 +31,4 @@ try {
 
 const goalCalsField = Math.round(goalIntervention.calories * ENERGYUNITS);
 
-console.log(goalCalsField);
+console.log(goalCalsField, unachievableGoal);
